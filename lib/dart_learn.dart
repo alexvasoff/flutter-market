b8e3252c566f97dea67c39c5b5994201e167c508
Aleksey Vasilev <alexvasoff@yandex.ru>
2020-09-14 21:46:18 +0300

    Все еще рассмотрение синтаксиса дарта

--- lib/dart_learn.dart
+++ lib/dart_learn.dart
@@ -1,5 +1,5 @@
 void main() {
-  functions();
+  collections();
 }
 
 void baseTypes() {
@@ -32,31 +32,86 @@ void functions() {
   print("\nФорич:");
   list.forEach(printValue);
 
-  list.forEach((element) {  // через анонимную ф-ю
+  list.forEach((element) {
+    // через анонимную ф-ю
     print(element);
     //printValue(element);
   });
 
   list.forEach((element) => print(element));
-  
+
   var toUp = (value) => "${value.toUpperCase()}";
   print(toUp("asd"));
 
   // ф-я с Именованными параметрами (параметры оборачиваются в {} ):
-  String getModel({String title, int value})
-  {
-    return "$title is ${value*2}";
+  String getModel({String title, int value}) {
+    return "$title is ${value * 2}";
   }
-  print( getModel(title: "Именованный параметр", value: 5) );
+
+  print(getModel(title: "Именованный параметр", value: 5));
 
   // Ф-я с Позиционными параметрами
-  String sayHello(String name, String msg, [String ps]) {
-    String res = "Hello, dear $name! ";
+  String sayHello(String name, String msg, [String ps = "How are you?"]) {
+    String res = "Hello, dear $name! $msg ";
     if (ps != null) res += ps;
     return res;
   }
 
-  print(sayHello("Alex", "123", "1222"));
+  print(sayHello("Alex", "123", null));
 
+  // Параметры по умолчанию
+  String trueName({String name = "Alex", bool correct = false}) {
+    return correct ? name : "Не верное имя!";
+  }
 
+  print(trueName());
+}
+
+// Замыкание
+void closures() {
+  Function addFunction(num add) {
+    return (num i) => add + i;
+  }
+
+  var add2 = addFunction(2);
+  Function add4 = addFunction(4);
+
+  print(add2(2));
+  print(add4(2));
+}
+
+// Коллекции
+void collections() {
+  // List - массив
+  List<int> myIntList = [0, 1, 2, 3];
+  var list = [4, 12, 5];
+  print("Исходный массив myList $myIntList");
+  myIntList.add(-5);
+  print("Добавили -5 в лист. $myIntList");
+  myIntList[0] = 10;
+  print("Change 1st element: $myIntList");
+  myIntList.removeAt(0);
+  print("Удалили 0 элемент: $myIntList");
+  myIntList.remove(-5);
+  print("Удаление элемение по значению: $myIntList");
+
+  print("\n");
+// spread оператор
+  var list1 = [1, 2, 3];
+  var list2 = [5, 10, ...list1];
+  print(list2);
+// nullable spread
+  var nullableList;
+  var list3 = [1, ...?nullableList];
+  print(list3);
+
+  bool permission = true;
+  var nav = ["home", "about", if (permission) "admin"];
+  print(nav);
+
+  List<int> intList = [1, 3, 5, 7, 2, 5];
+  List<String> strList = [for(var i in intList) ">$i"];
+  List<int> generator = [for (int i = 0; i<10; i++) i];
+  print(strList);
+  print(generator);
 }
