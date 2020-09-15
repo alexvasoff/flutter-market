df8fafa91dadfb79e826286ebf404b90e10429cd
Aleksey Vasilev <alexvasoff@yandex.ru>
2020-09-15 09:56:18 +0300

    Создание виджета Item (товар)

---
+++ lib/test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+
+}
+
+class Item extends StatelessWidget {
+
+  final String name;
+  final String img;
+  final String description;
+  final double price;
+
+  Item(this.name, this.description, this.price, this.img);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
