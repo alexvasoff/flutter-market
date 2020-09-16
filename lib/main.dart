52d55010b75e9bfb05cba22e3b278b46e0248379
Aleksey Vasilev <alexvasoff@yandex.ru>
2020-09-16 19:38:55 +0300

    Создание основного каркаса приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,25 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Shop",
-      home: Shop(),
+      title: "Title of application!",
+      home: HomePage(),
     );
   }
 }
 
-class Shop extends StatefulWidget {
-  @override
-  _ShopState createState() => _ShopState();
-}
-
-class _ShopState extends State<Shop> {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Widget Store"),
+        title: Text("Классное название!"),
       ),
-      body: _buildItems(),
-      );
-  }
-}
-
-Widget _buildItems() {
-  return ListView(
-    padding: EdgeInsets.all(8.0),
-    children: itemRep,
-
-  );
-}
-
-
-Widget itemRepresent(int index) {
-  return ListTile(
-    title: Text(items[index].name),
-    subtitle: Text(items[index].description),
-    leading: Icon(Icons.picture_in_picture),
-  );
-
-
-
-}
-
-List<Widget> itemRep = [for (int i = 0; i < items.length; i++) itemRepresent(i)];
-
-
-List<Item> items = [Item("1", "is one", 15,"111"), Item("2", "123", 5, "555")];
-
-
-class Item extends StatelessWidget {
-
-  final String name;
-  final String img;
-  final String description;
-  final double price;
-
-  Item(this.name, this.description, this.price, this.img);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text("$name это имя");
+      body: Center(child: Container(child: Text("Добрый день!"))),
+    );
   }
 }
---
+++ lib/models.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class Item {
+  final String name;
+  final String description;
+  final num price;
+  final String img;
+
+  Item(this.name, this.description, this.price, this.img);
+
+}
+
