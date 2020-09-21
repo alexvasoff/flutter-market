53041678cdf5421b16ecf22ae8c20d0490134ba6
Aleksey Vasilev <alexvasoff@yandex.ru>
2020-09-21 18:22:15 +0300

    Порядок в проете (learn)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_learn/random_word.dart';
+import 'package:flutter_learn/tutorial_home.dart';
 
 void main() => runApp(MyApp()); // вызов класса MyApp на старте
 
@@ -10,86 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: RandomWords(),
-    );
-  }
-}
-
-// stful
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestion = <WordPair>[]; // массив для хранения слов
-  final _biggerFont = TextStyle(fontSize: 18.0);
-  final _saved = Set<WordPair>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      //панель приложения по умолчанию
-      appBar: AppBar(
-        title: Text(
-          "Генератор Названий",
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd)
-          return Divider(height: 16.0); // если нечет - создание разделителя
-
-        final index = i ~/ 2;
-        if (index >= _suggestion.length) {
-          _suggestion.addAll(generateWordPairs()
-              .take(10)); // добавить 10 сгенеренных пар в лист
-        }
-        return _buildRow(_suggestion[index], index);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair, int number) {
-    String subText;
-    final bool alreadySaved = _saved.contains(pair);
-    if (alreadySaved) {
-      subText = "Хороший выбор!";
-    } else
-      subText = "Нравится?";
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      // иконка в конце списка
-      subtitle: Text(subText),
-      leading: Text(
-        number.toString(),
-        style: TextStyle(fontSize: 21),
-      ),
-      onTap: () {
-        setState(() {
-          // Сообщите, что внутреннее состояние объекта изменилось
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
+      home: TutorialHome(),
     );
   }
 }
---
+++ lib/tutorial_home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.picture_in_picture),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}
