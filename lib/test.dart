32c354cf67a17049efd3f5b9885abb9b8c48d80a
Aleksey Vasilev <alexvasoff@yandex.ru>
2020-09-27 22:16:10 +0300

    Выживший файл после огромного отката :(

--- lib/test.dart
+++ lib/test.dart
@@ -1,57 +1,46 @@
 import 'dart:convert';
 import 'dart:io';
 
-main() async {
-  //String data = File('assets/ItemsData').readAsStringSync(); // чтение файла
-  //print(data);
-
-  // Map x = jsonDecode(await File('assets/ItemsData').readAsString());
-  //String res =
-  // Student.toJson(Student("Bob", "212@sampe.com", DateTime(2015)));
-  // print(res);
-  print(Student.fromJsonArray(jsonData));
+main() {
+  print(ItemsStorage.getIntance().items);
 }
 
 // пример кодирования в JSON
-class Student {
+class Item {
   final String name;
-  final String email;
-  final DateTime dob;
+  final String description;
+  final num price;
+  final String img;
 
-  Student(this.name, this.email, this.dob);
-
-  static String toJson(Student s) {
-    Map<String, dynamic> map() =>
-        {'name': s.name, 'email': s.email, 'dob': s.dob.toIso8601String()};
-    String result = jsonEncode(map());
-
-    return result;
-  }
-
-  static Student fromJsonMap(Map<String, dynamic> json) {
-    print('json object $json');
-    String name = json["name"];
-    String email = json["email"];
-    DateTime dob = DateTime.parse(json["dob"]);
-    Student s = new Student(name, email, dob);
-    return s;
-  }
-
-  static List<Student> fromJsonArray(String jsonString) {
-    Map<String, dynamic> decodeMap = jsonDecode(jsonString);
-    print('Decode map $decodeMap');
-    List<dynamic> dynamicList = decodeMap["students"];
-    print('dynamicList $dynamicList');
-    List<Student> students = new List<Student>();
-    dynamicList.forEach((element) {
-      Student s = Student.fromJsonMap(element);
-      print('student $s');
-      students.add(s);
-    });
-    return students;
-  }
+  Item(this.name, this.description, this.img, this.price);
 }
 
-String jsonData =
-    '{    "students": [{"name": "John","email": "john@example.com","dob": "2000-02-14T00:00:00.000"},{"name": "Jane","email": "jane@example.com","dob": "1999-03-01T00:00:00.000"}]}';
-// Map<String, dynamic> json = jsonDecode(jsonStr)
+class ItemsStorage {
+  ItemsStorage._internal();
+  static ItemsStorage _itemsStotage = ItemsStorage._internal();
+  static ItemsStorage getIntance() => _itemsStotage;
+
+  static Item fromJsonMap(Map<String, dynamic> json) {
+    String name = json["name"];
+    String description = json["description"];
+    num price = json["price"];
+    String img = json["img"];
+
+    Item i = new Item(name, description, img, price);
+    return i;
+  }
+
+  static List<Item> fromJsonArray(String jsonString) {
+    Map<String, dynamic> decodeMap = jsonDecode(jsonString);
+    List<dynamic> dynamicList = decodeMap["Items"];
+    List<Item> items = new List<Item>();
+    dynamicList.forEach((element) {
+      Item i = ItemsStorage.fromJsonMap(element);
+      items.add(i);
+    });
+    return items;
+  }
+
+  List<Item> get items =>
+      ItemsStorage.fromJsonArray(File("assets/ItemsData").readAsStringSync());
+}
