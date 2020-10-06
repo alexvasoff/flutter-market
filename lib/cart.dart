f2854291dc44c5647c875ec5caa1d8cdba44f018
Aleksey Vasilev <alexvasoff@yandex.ru>
2020-10-06 09:43:21 +0300

    Добавление кнопки корзины в бар

---
+++ lib/cart.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('KORZINA'),
+      ),
+      body: Text("Basket!"),
+    );
+  }
+}
